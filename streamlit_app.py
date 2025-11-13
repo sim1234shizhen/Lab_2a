eba7082bf14e4d6766341ffce00ebd69c39010aa
sim1234shizhen <simshizhen@gmail.com>
2025-11-13 15:28:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Hi")
+st.title("🎈 Bye")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
